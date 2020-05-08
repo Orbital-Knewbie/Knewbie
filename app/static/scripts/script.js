1c21bd6a0c942b479bff6e001a838a3709cce71a
R Ramana <ramanalfc@yahoo.com.sg>
2020-05-08 20:26:56 +0800

    update style for login

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -1,34 +1,5 @@
 $(document).ready(function () {
 
-  // TRANSITION FOR SPLIT SCREEN
-  const left = document.querySelector('.left');
-  const right = document.querySelector('.right');
-  const container = document.querySelector('.split-container');
-
-  left.addEventListener('mouseenter', () => {
-    container.classList.add('hover-left');
-  });
-
-  left.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-left');
-  });
-
-  right.addEventListener('mouseenter', () => {
-    container.classList.add('hover-right');
-  });
-
-  right.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-right');
-  });
-
-  // LOADING PAGE
-  // $(window).on("load", function(){
-  //   $(".loader-wrapper").fadeOut("fast");
-  //   $("#name").addClass("animateName");
-  //   $("#sub").addClass("animateSub");
-  //   $('body').removeClass('loading');
-  // });
-
   // NAVBAR TOGGLE OPEN AND CLOSE
   $('.menu').on('click', function () {
     $(this).toggleClass('open');
@@ -45,23 +16,9 @@ $(document).ready(function () {
   gsap.fromTo('.logo', {x: -200, opacity: 0}, {duration: 1, delay: 0.5, x: 0, opacity: 1});
   gsap.fromTo('.menu', {opacity: 0}, {duration: 2, delay: 0.5, opacity: 1});
   gsap.fromTo('.gradient-textbox', {yPercent: 40, opacity: 0}, {duration: 1, delay: 0.8, yPercent: -50, opacity: 1});
-
-  //ANIMATE ON SCROLL QUERY
-  //initialize
-  // AOS.init({
-  //   easing: 'ease',
-  //   duration: 1800,
-  //   once: true
-  // });
-
-
 });
 
 //SMOOTHSCROLL FOR NAVBAR
-// const scroll = new SmoothScroll('.navbar a[href*="#"]', {
-//   speed: 1500
-// });
-//
 $('.navbar a').on('click', function (e) {
   if (this.hash !== '') {
     e.preventDefault();
@@ -101,14 +58,14 @@ document.querySelector('.close1').addEventListener("click", function() {
 });
 
 // MODAL FOR LOGIN
-document.getElementById('std-login').addEventListener("click", function () {
-   document.querySelector('.login-modal').style.display = "flex";
-});
+//document.getElementById('std-login').addEventListener("click", function () {
+//   document.querySelector('.login-modal').style.display = "flex";
+//});
 
-document.getElementById('tcr-login').addEventListener("click", function () {
-   document.querySelector('.login-modal').style.display = "flex";
-});
+//document.getElementById('tcr-login').addEventListener("click", function () {
+//   document.querySelector('.login-modal').style.display = "flex";
+//});
 
-document.querySelector('.loginclose').addEventListener("click", function () {
-   document.querySelector('.login-modal').style.display = "none";
-});
+//document.querySelector('.loginclose').addEventListener("click", function () {
+//   document.querySelector('.login-modal').style.display = "none";
+//});
