61569fec4ec52fda3f10676fc7ca0e5fbad711eb
Wira Azmoon <wira.azmoon@gmail.com>
2020-06-27 01:36:17 +0800

    install

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,9 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 
+from install import org_qns
+from catsim.cat import generate_item_bank
+
 naming_convention = {
     "ix": 'ix_%(column_0_label)s',
     "uq": "uq_%(table_name)s_%(column_0_name)s",
@@ -28,7 +31,43 @@ login.login_view = 'login'
 mail = Mail(app)
 
 from app import models
+
 db.create_all()
+
+def add_qn(org_qns):
+    '''Adds questions to the database, where questions are formatted to be in a dictionary
+    {<question>:{'answer':<options>,'difficulty':<difficulty>}
+    <questions> is str
+    <options> is list of str
+    <difficulty> is float (not added yet)
+    '''
+    
+    for q in org_qns.keys():
+        item = generate_item_bank(1)[0]
+        qn = models.Question(question=q, discrimination=item[0], \
+                    difficulty=item[1], guessing=item[2], upper=item[3], topicID=1)
+        db.session.add(qn)
+        db.session.commit()
+        qid = qn.id
+        b=True
+        for o in org_qns[q]['answers']:
+            opt=models.Option(qnID=qid,option=o)
+            db.session.add(opt)
+            if b:
+                db.session.flush()
+                qn.answerID = opt.id
+                
+                b=False
+            
+            db.session.commit()
+
+if db.session.query(models.Topic).count() == 0:
+    for topic in ('General', 'Estimation', 'Geometry', 'Model'):
+        db.session.add(models.Topic(name=topic))
+if db.session.query(models.Question).count() == 0:
+    add_qn(org_qns)
+
+
 from app import views
 from app.errors.handlers import errors
 
@@ -36,3 +75,6 @@ app.register_blueprint(errors)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
--- app/questions.py
+++ app/questions.py
@@ -360,120 +360,11 @@ def add_test_topics():
     for topic in topics:
         add_topic(topic)
 
-add_test_topics()
+#add_test_topics()
 
-org_qns = {
-    "Fill in the blank: 423 x 1000 = ____ x 10": {
-        "answers": [
-            "42300",
-            "423",
-            "4230",
-            "423000"
-        ],
-        "difficulty": "Easy"
-    },
-    "Which of the following is closest to 1?": {
-        "answers": [
-            "4/5",
-            "1/2",
-            "2/3",
-            "3/4"
-        ],
-        "difficulty": "Easy"
-    },
-    "Which of the following is the same as 2010 g?": {
-        "answers": [
-            "2 kg 10 g",
-            "2 kg 1 g",
-            "20 kg 1 g",
-            "20 kg 10 g"
-        ],
-        "difficulty": "Easy"
-    },
-    "Find the value of (3y +1) / 4 , when y = 5": {
-        "answers": [
-            "4",
-            "9",
-            "8",
-            "5"
-        ],
-        "difficulty": "Easy"
-    },
-    "The ratio of Rachel's age to Samy's age is 7 : 6.  Rachel is 4 years older than Samy. What is Samy's age this year?": {
-        "answers": [
-            "24",
-            "10",
-            "11",
-            "28"
-        ],
-        "difficulty": "Easy"
-    },
-    "How many fifths are there in 2 3/5?": {
-        "answers": [
-            "13",
-            "11",
-            "3",
-            "6"
-        ],
-        "difficulty": "Easy"
-    },
-    "The sum of 1/2 and 2/5 is the same as ___.": {
-        "answers": [
-            "0.900",
-            "0.009",
-            "0.090",
-            "9.000"
-        ],
-        "difficulty": "Easy"
-    },
-    "What is the value of 36 + 24 / (9 - 3) + 2 x 5 ?": {
-        "answers": [
-            "50",
-            "51",
-            "60",
-            "66"
-        ],
-        "difficulty": "Easy"
-    },
-    "Sally has 100 marbles and her brother has 300 marbles. Express Sally's marbles as a percentage of the total number of marbles they have altogether.": {
-        "answers": [
-            "25%",
-            "33 1/3 %",
-            "300%",
-            "400%"
-        ],
-        "difficulty": "Easy"
-    },
-    "Mr Tan has an equal number of pens and pencils. He puts the pens in bundles of 8 and the pencils in bundles of 12. There are 15 bundles altogether. How many pens are there?": {
-        "answers": [
-            "72",
-            "24",
-            "48",
-            "96"
-        ],
-        "difficulty": "Hard"
-    },
-    "3 ones, 4 tenths and 7 thousandths is ___": {
-        "answers": [
-            "3.407",
-            "3.470",
-            "3.047",
-            "3.740"
-        ],
-        "difficulty": "Hard"
-    },
-    "Which of the following is equal to 0.25%?": {
-        "answers": [
-            "1/400",
-            "1/25",
-            "1/4",
-            "25"
-        ],
-        "difficulty": "Hard"
-    }
-}
 
-add_qn(org_qns)
+
+#add_qn(org_qns)
 
 def clear_questions():
     for q in Question.query.all():
@@ -488,7 +379,7 @@ def clear_responses():
         db.session.delete(r)
     db.session.commit()
 
-clear_responses()
+#clear_responses()
 
 def test_insert_qns():
     '''To test insert_qns() works'''
